ca463060174332914a35ff0528d678148d7e5f3e
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-11-03 23:10:00 -0400

    Initial release

---
+++ DESCRIPTION.md
@@ -0,0 +1,24 @@
+
+---
+
+### [choco://crushee.install](choco://crushee.install)
+
+To use choco:// protocol URLs, install [(unofficial) choco:// Protocol support](https://community.chocolatey.org/packages/choco-protocol-support)
+
+---
+
+## Crushee
+
+Crushee is an image compression tool, powered by Electron and Node.js. You can carefully tune your images locally to meet your needs, without having to re-upload to a web service, or restart an application every time.
+
+![Screenshot](https://cdn.jsdelivr.net/gh/brogers5/chocolatey-package-crushee.install@899fd1d5ccc784374ecd07d819958fa2f6f4f9a8/Screenshot.png)
+
+### Features
+
+* Input several different image file formats (e.g. JPEG, PNG, GIF, SVG, WebP, AVIF, HEIC)
+* Optionally convert to select image file formats (e.g. JPEG, PNG, WebP, AVIF)
+* Resize images - define an exact size, or upscale/downscale while preserving the original aspect ratio
+* Batch processing - analyze, crush and save multiple images at once!
+* Quality Level sliders - experiment and find the perfect balance between picture quality and file size!
+* Advanced format-specific options, such as Chroma Subsampling Level and Color Count
+* Light/Dark Mode app theme
---
+++ helpers.ps1
@@ -0,0 +1,49 @@
+﻿Import-Module PowerShellForGitHub
+
+$installerFileNameRegex = 'Crushee-v([\d\.]+)-win-setup\.exe$'
+$owner = 'xanderfrangos'
+$repository = 'crushee'
+
+function Get-LatestStableVersion {
+    $latestRelease = (Get-GitHubRelease -OwnerName $owner -RepositoryName $repository -Latest)[0]
+
+    return [Version] $latestRelease.tag_name.Substring(1)
+}
+
+function Get-SoftwareUri {
+    [CmdletBinding()]
+    param(
+        [Version] $Version
+    )
+
+    if ($null -eq $Version)
+    {
+        # Default to latest stable version
+        $release = (Get-GitHubRelease -OwnerName $owner -RepositoryName $repository -Latest)[0]
+    }
+    else 
+    {
+        $release = Get-GitHubRelease -OwnerName $owner -RepositoryName $repository -Tag "v$($Version.ToString())"
+    }
+    $releaseAssets = Get-GitHubReleaseAsset -OwnerName $owner -RepositoryName $repository -Release $release.ID
+
+    $windowsInstallerAsset = $null
+    foreach ($asset in $releaseAssets)
+    {
+        if ($asset.name -match $installerFileNameRegex)
+        {
+            $windowsInstallerAsset = $asset
+            break;
+        }
+        else {
+            continue;
+        }
+    }
+
+    if ($null -eq $windowsInstallerAsset)
+    {
+        throw "Cannot find published Windows installer asset!"
+    }
+
+    return $windowsInstallerAsset.browser_download_url
+}
